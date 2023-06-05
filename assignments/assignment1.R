8cfe3dd19487ebda1aaff2d5414a6e818bcbc1f3
Oghenetejiri26 <oghenetejiri.umweni@northumbria.ac.uk>
2023-06-05 14:20:57 +0100

    first code

--- assignments/assignment1.R
+++ assignments/assignment1.R
@@ -1,2 +1,13 @@
-setwd(“~/Desktop/Msc Project”)
+setwd("~/Desktop/Msc Project")
 x = 15
+result <- runif(1)*50 #Random number betwen 0 and 50
+if (result < 5){
+    print("lower")
+}else{
+    print("higher")
+}
+if(x < 20){
+    print("teejay")
+}else{
+    print("NO")
+}
