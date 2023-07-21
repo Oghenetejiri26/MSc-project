7c84016726703463f6a08de95b72cf5a0c182d66
Oghenetejiri26 <oghenetejiri.umweni@northumbria.ac.uk>
2023-07-21 14:48:44 +0100

    Creating models across the mouse timeline

--- Projects/Model.R
+++ Projects/Model.R
@@ -25,7 +25,7 @@ toSeason <- function(month) {
  toSeason(6)
  strsplit(phenotypes[, "W.dat"], ".", fixed=TRUE)
 
- splitted <- strsplit(offspring[, "W.dat"], ".", fixed=TRUE)
+ splitted <- strsplit(children[, "W.dat"], ".", fixed=TRUE)
  lapply(splitted, "[", 2)
  unlist(lapply(splitted, "[", 2))
  bmonths <- as.numeric(unlist(lapply(splitted, "[", 2)))
@@ -42,6 +42,7 @@ cage <- children[, "Kerb"]
 color <- children[, "Farbe"]
 father <- children[, "Vater"]
 mother <- as.factor(children[, "Mutter"])
+season <- as.factor(children[, "bSeason"])
 
 #Create models 
 m1 <- lm(Y ~ mother)
@@ -70,41 +71,64 @@ AIC(m3,m4)#Conclusion:
 
 #Conclusion: The minimal model at 211 days is: lm(Y ~ mother + as.factor(littersize))
 
-
+# build a model without the mother because 1/2 of your genome comes from the mother
 unique(father)
 unique(mother)
 
-m1 <- lm(Y ~ mother + as.factor(littersize))
-m2 <- lm(Y ~ mother + as.factor(littersize) + as.factor(parity))
+m1 <- lm(Y ~  as.factor(littersize))
+m2 <- lm(Y ~  as.factor(littersize) + as.factor(parity))
 AIC(m1,m2)
 
-m2 <- lm(Y ~ mother + as.factor(littersize) + as.factor(season))
-AIC(m1.m2)
+m2 <- lm(Y ~  as.factor(littersize) + as.factor(season))
+AIC(m1,m2)
 
-# Conclusion: The minimal model at 21 days is lm(Y ~ mother + as.factor(littersize) + as.factor(season))
+# Conclusion: The minimal model at 21 days is lm(Y ~  as.factor(littersize) + as.factor(season))
 
 genotypes = read.table("genotypes_gen28.txt", sep = "\t", check.names = FALSE )
-map <- genotypes[,1;3]
-genotypes <- genotypes[,-c(1;3)]
+map <- genotypes[,1:3]
+genotypes <- genotypes[,-c(1:3)]
 
-children <- [colnames(genotypes),]
+children <- children[colnames(genotypes),]
 
-Y <- children[, "d21"]
-littersize <- as.factor[children[, "WG"]]
-season <- as.factor[children[, "bSeason"]]
-mother <- as.factor[children[, "Mutter"]]
+for(day in c("d21","d28","d35","d42","d49","d56","d63","d70")){
+Y <- children[,day]
+littersize <- as.factor(children[, "WG"])
+mother <- as.factor(children[, "Mutter"])
+season <- as.factor(children[, "bSeason"])
 
 pV <- c()
 for(x in 1:nrow(genotypes)){
-  marker <- factor(geneotypes[x,], levels = c("A", "H", "B"))
+  marker <- factor(genotypes[x,], levels = c("A", "H", "B"))
   if(length(na.omit(unique(marker))) > 1) {
     mm <- lm(Y ~ mother + littersize + marker)
-    mnm <- lm(Y ~ littersize + season + mareker)
+    mnm <- lm(Y ~ littersize + season + marker)
     pM <- anova(mm)["marker", "Pr(>F)"]
     pNM <- anova(mnm)["marker", "Pr(>F)"]
     pV  <- rbind(pV, c(pM, pNM))
   }else{
     pV <-rbind(pV, c(NA,NA))
   }
+  cat("Scanning", x, "\n")
 }
-LODs <- log10(pV)
+
+rownames(pV) <- rownames(genotypes)
+colnames(pV) <- c("Model1", "Model2")
+write.table(pV,file = paste0("pvalues_", day, ".txt"),
+            sep="\t", quote=FALSE, na= "")
+}            
+LODs <- -log10(pV)
+m1bf <- p.adjust(pV[,1], "Bonferroni")
+b1bh <- p.adjust(pV[,1], "BH")
+threshold = -log10(0.05) #bonferonni correction
+
+LODs_m1bf <- -log10(m1bf)
+LODs_m1bh <- -log10(b1bh)
+
+#plot(LODs[,1])
+#plot(LODs[,2])
+
+plot(LODs_m1bh, col = "green", pch=18)
+points(LODs_m1bf, col = "red", pch=18)
+albine(h = threshold)
+
+map[which(LODs_m1bh > threshold),]
